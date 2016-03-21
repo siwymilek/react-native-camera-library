0714b3cff60fc7266700d949c0726afc461028aa
Piotr Miłoszewicz <peter@omega323.com>
2016-03-21 16:57:54 +0100

    release 0.0.3 code cleanup

--- CameraLibrary.m
+++ CameraLibrary.m
@@ -14,69 +14,61 @@ RCT_EXPORT_METHOD(getPhotos:(NSDictionary *)props callback:(RCTResponseSenderBlo
     options.includeAssetSourceTypes = PHAssetSourceTypeUserLibrary;
     
     PHFetchResult *allPhotosResult = [PHAsset fetchAssetsWithOptions:options];
-//    PHFetchResult *allPhotosResult = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeVideo options:nil];
     NSMutableArray *collection = [NSMutableArray array];
-    
-    PHImageRequestOptions *requestOptionForPhotos = [[PHImageRequestOptions alloc] init];
-    requestOptionForPhotos.synchronous = YES;
-//    requestOptionForPhotos.deliveryMode = PHImageRequestOptionsDeliveryModeFastFormat;
-    
-    PHVideoRequestOptions *requestOptionForVideos = [[PHVideoRequestOptions alloc] init];
-    
+
     BOOL nextPage = YES;
-    
+
     int countObjects = (int)[allPhotosResult count];
-    
+
     int perPage = 20;
     int page = [[props valueForKey:@"page"] intValue];
     int lastPage = ceil(countObjects/perPage);
-    
+
 //    int from = (page-1)*perPage;
 //    int to = (page*perPage)-1;
-    
 //    if(to > countObjects) {
 //        to = countObjects;
 //        nextPage = NO;
 //    }
-    
+
     int from = countObjects-((page-1)*perPage)-1;
     int to = countObjects-(page*perPage);
     if(to < 0) {
         to = 0;
     }
-    
+
     if(to == 0) {
         nextPage = NO;
     }
-    
+
     for(int i = from; i >= to; i--) {
         PHAsset *asset = [allPhotosResult objectAtIndex:i];
         NSMutableDictionary *item = [NSMutableDictionary dictionary];
-        
+
         if([asset mediaType] == 1) {
             //Photo
-            
+
             [[PHImageManager defaultManager]
              requestImageForAsset:asset
              targetSize:CGSizeMake(80, 80)
              contentMode:PHImageContentModeAspectFill
-             options:requestOptionForPhotos
+             options:nil
              resultHandler:^(UIImage *result, NSDictionary *info) {
-                 
+
                  NSData *data = UIImagePNGRepresentation(result);
                  NSString *base = [data base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
-                 
+
                  if(base != nil) {
                      NSString *filename = [asset.localIdentifier substringWithRange:NSMakeRange(0, 36)];
                      NSString *url = [NSString stringWithFormat:@"assets-library://asset/asset.JPG?id=%@&ext=JPG", filename];
-                     
+
                      [item setValue:url forKey:@"url"];
                      [item setValue:base forKey:@"thumbnail"];
                      [item setValue:@"photo" forKey:@"type"];
-                     
+
                      [collection addObject:item];
                  }
-                 
+
                  if(i == to) {
                      callback(@[@{
                                     @"objects": collection,
@@ -88,9 +80,9 @@ RCT_EXPORT_METHOD(getPhotos:(NSDictionary *)props callback:(RCTResponseSenderBlo
              }];
         } else if([asset mediaType] == 2) {
             // Video
-            
+
             [[PHImageManager defaultManager]
-             requestAVAssetForVideo:asset options:requestOptionForVideos
+             requestAVAssetForVideo:asset options:nil
              resultHandler:^(AVAsset * _Nullable result, AVAudioMix * _Nullable audioMix, NSDictionary * _Nullable info) {
                  
                  AVAssetImageGenerator *gen = [[AVAssetImageGenerator alloc] initWithAsset:result];
